963f3a0e0d8c51641cacfeccd7e834f998798761
koralle <mkhd7930@gmail.com>
2022-09-28 13:45:19 +0900

    fix

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,17 @@ import type { Config } from '@jest/types';
 
 // Export Jest Config
 const config: Config.InitialOptions = {
-  verbose: true
-} as const
+  verbose: true,
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "@/(.*)$": "<roorDir>/src/$1"
+  }
+}
 
 export default config
